76b34f5f0f57d7920f633e90b745f2a273ab516c
fitocs452 <fitomm@hotmail.es>
2016-03-31 18:49:02 -0600

    Agregar JAR de antlr4

--- src/dbms/VisitorAdolfo.java
+++ src/dbms/VisitorAdolfo.java
@@ -5,10 +5,31 @@
  */
 package dbms;
 
+import antlr.sqlBaseVisitor;
+import antlr.sqlParser;
+import org.antlr.v4.runtime.misc.NotNull;
+
 /**
  *
  * @author adolfo
  */
-public class VisitorAdolfo {
+public class VisitorAdolfo<T> extends sqlBaseVisitor{
     
+    private FileManager manejador = new FileManager();
+    public String bdActual ="";
+    
+    @Override
+    public T visitInsert(@NotNull sqlParser.InsertContext ctx) {
+        return (T)visitChildren(ctx);
+    }
+    
+    @Override
+    public Object visitUse_schema_statement(sqlParser.Use_schema_statementContext ctx) {
+        
+        bdActual = ctx.getChild(2).getText();
+       
+        if (manejador.checkDB(bdActual)==false)
+            bdActual = "";
+        return super.visitUse_schema_statement(ctx); //To change body of generated methods, choose Tools | Templates.
+    }
 }
