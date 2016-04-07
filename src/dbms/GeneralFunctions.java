453c4a8e2ca9d18657cab346e5a7df9363fd2788
fitocs452 <fitomm@hotmail.es>
2016-04-07 17:13:27 -0600

    Funcion para imprimir data en JTable

---
+++ src/dbms/GeneralFunctions.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbms;
+
+import static dbms.ANTGui.jTable1;
+import java.util.ArrayList;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author adolfo
+ */
+public class GeneralFunctions {
+    public JTable generarTabla(ArrayList<ArrayList> datos, ArrayList nombreColumnas) {
+        
+        DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
+        model.setRowCount(0);
+        // Nombres de columna
+        model.setColumnIdentifiers(nombreColumnas.toArray());
+
+        for(int i = 0; i < datos.size(); i++) {
+            model.addRow(datos.get(i).toArray());
+        }
+        
+        return null;
+    }
+}
